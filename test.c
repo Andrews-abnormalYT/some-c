dabb780a8faf381684f6ad7121c15ce2e6aa3e0b
Android <115855973+Andrews-abnormalYT@users.noreply.github.com>
2023-11-05 16:48:16 +1030

    Update test.c

--- test.c
+++ test.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h> // idk if this is a real library...
+
 
 int main() {
   printf("Hello World!");
   printf("Hi people");
+  bool ISuckAtC = True;
+
+  if (ISuckAtC == true) {
+    printf(true);
+  } else {
+    printf(0);
+  }
+  
+  
   return 0;
 }
